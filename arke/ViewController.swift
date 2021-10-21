859b67bb552b2ab434ac742bd1e6440f6aebf102
leonarrdo404 <leo@ese.plus>
2021-10-20 21:59:55 -0600

    xmas tree reality kit project

--- arke/ViewController.swift
+++ arke/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import RealityKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var arView: ARView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let xmasTree = try! ModelEntity.load(named: "Xmas_tree")
+        let anchor = AnchorEntity(plane: .horizontal)
+        anchor.addChild(xmasTree)
+        
+        arView.scene.addAnchor(anchor)
     }
-
-
 }
 
