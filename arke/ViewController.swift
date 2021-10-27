ef121da852e475a182c829f2435fd46841c3f506
leonarrdo404 <leo@ese.plus>
2021-10-26 22:49:59 -0600

    enable gestures

--- arke/ViewController.swift
+++ arke/ViewController.swift
@@ -10,15 +10,38 @@ import RealityKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var arView: ARView!
-
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let xmasTree = try! ModelEntity.load(named: "Xmas_tree")
-        let anchor = AnchorEntity(plane: .horizontal)
-        anchor.addChild(xmasTree)
+        let modelEntiy = ModelEntity()
+        let anchorEntity = AnchorEntity(plane: .horizontal)
+       
+        modelEntiy.addChild(xmasTree)
+        anchorEntity.addChild(modelEntiy)
         
-        arView.scene.addAnchor(anchor)
+        // Gestures
+        modelEntiy.generateCollisionShapes(recursive: true)
+        arView.installGestures([.translation, .rotation, .scale], for: modelEntiy)
+
+        arView.scene.addAnchor(anchorEntity)
     }
 }
 
+extension ViewController {
+    func enableObjectRemoval() {
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        
+        arView.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        
+        if let entity = arView.entity(at: location) {
+            
+        }
+    }
+}
